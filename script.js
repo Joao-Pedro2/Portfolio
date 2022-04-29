24eed441f61e1904837ba7256da374f26672330f
Joao-Pedro2 <joaopedrosousaprofissional@gmail.com>
2022-04-29 11:46:14 -0300

    att responsive

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//controls menubar
 function openMenu(){
   if(document.querySelector('.menu').classList.contains = 'menu'){
     document.querySelector('.menu').classList.add('menu-open')
@@ -10,3 +12,13 @@ function closeMenu(){
     document.querySelector('.close-menu').classList.remove('close')
   }
 }
+
+//controls slider
+
+let totalSlides = document.querySelectorAll('.slider-itens').length;
+
+document.querySelector('.slider').style.width = 'calc(100vw * ${totalSlides})';
+
+document.querySelector('.controls').style.height = 
+`${document.querySelector('.slider-container').clientHeight}px`;
+
